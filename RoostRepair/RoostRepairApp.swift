26efc05e74e339aa8e5333138b0aa2f5a5f39fc7
Allchaingf <ulymarina0909@gmail.com>
2026-07-02 19:57:52 +0200

    Initial Commit

---
+++ RoostRepair/RoostRepairApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoostRepairApp.swift
+//  RoostRepair
+//
+//  Created by Andres Ivanov on 02/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct RoostRepairApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
